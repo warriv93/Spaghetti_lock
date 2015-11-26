2f875ac53e01dd53be419acb3a294114843b2e22
Tobi <se14m026@technikum-wien.at>
2015-11-26 22:15:31 +0900

    Basic bluetooth connection works now (python code)

--- pc/bt_test.py
+++ pc/bt_test.py
@@ -1,15 +1,18 @@
 from bluetooth import *
+import hashlib
 
+accepted_clients = ['C0:EE:FB:20:CC:51']
+
+# Create the RFCOMM bluetooth socket using any port number.
 server_sock=BluetoothSocket( RFCOMM )
 server_sock.bind(("",PORT_ANY))
 server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
 
-#uuid = "00001101-0000-1000-8000-00805F9B34FB"
 uuid = "fa87c0d0-afac-11de-8a39-0800200c9a66"
 
-advertise_service( server_sock, "moms_spaghetti",
+advertise_service( server_sock, "spaghetti_lock",
     service_id = uuid,
     service_classes = [ uuid, SERIAL_PORT_CLASS ],
     profiles = [ SERIAL_PORT_PROFILE ]
@@ -17,19 +20,38 @@ advertise_service( server_sock, "moms_spaghetti",
 
 print "Waiting for connection on RFCOMM channel %d" % port
 
-client_sock, client_info = server_sock.accept()
-print "Accepted connection from ", client_info
-
 try:
-    while True:
-        data = client_sock.recv(1024)
-        if len(data) == 0: break
-        print "received [%s]" % data
-except IOError:
-    pass
+  # Enter the while loop and listen for connections
+	while True:
+		client_sock, client_info = server_sock.accept()
+		
+		# Check the client address. If we don't know it, we just close the connection
+		if not any(client_info[0] in s for s in accepted_clients): 
+			print "Denied connection from ", client_info
+			client_sock.close()
+			continue
+		
+		# Ok, we got a connection request. Accept it and send welcome message.
+		print "Accepted connection from ", client_info
+		client_sock.settimeout(10.0)
+		
+		try:
+				client_sock.send('#Hello! What\'s the magic word?')
+				while True:
+						data = client_sock.recv(1024)
+						if len(data) == 0: break
+						print "received [%s]" % data
+						client_sock.send(data)
+		except IOError:
+				pass
 
-print "disconnected"
+		print "disconnected", client_info
 
-client_sock.close()
-server_sock.close()
+		client_sock.close()
+	
+except KeyboardInterrupt:
+	# Close socket and quit
+	stop_advertising(server_sock)
+	server_sock.close()
+
 print "all done"
